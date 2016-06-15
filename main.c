20b2dc288a594720a891afd87befc68e2860c157
navidrct <navidrct@gmail.com>
2016-06-15 18:04:07 +0330

    Signed-off-by: navidrct <navidrct@gmail.com>

--- main.c
+++ main.c
@@ -41,6 +41,7 @@ int main(void)
         nrf_gpio_pin_set(LED_1);
 				//man navid hastam
 			//its not working right
+			//i think i get it right
         nrf_delay_ms(500);
 
         nrf_gpio_pin_clear(LED_1);
